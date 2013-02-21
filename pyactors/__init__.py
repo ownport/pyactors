d8ee87e24c3f2c89564ae8602a7c403c4586555f
ownport <ownport@gmail.com>
2013-02-21 22:33:51 +0200

    changed to new v0.2.8

--- pyactors/__init__.py
+++ pyactors/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Andrey Usov <https://github.com/ownport/pyactors>'
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 __license__ = """
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
