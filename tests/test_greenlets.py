f2882f74bc89af2a95d5694e93cb1eb9da994692
ownport <ownport@gmail.com>
2013-03-13 22:50:50 +0200

    refactoring imap function

--- tests/test_greenlets.py
+++ tests/test_greenlets.py
@@ -21,7 +21,7 @@ def test_gevent_imap():
     queue = Queue()
     for i in range(5):
         queue.put('msg-%d' % i)
-    imap = imap_nonblocking(3, simple_task, queue)        
+    imap = imap_nonblocking(simple_task, queue, 3)        
     result = [msg for msg in imap if msg is not None]
     assert result == ['msg-0:handled','msg-1:handled','msg-2:handled',
                       'msg-3:handled','msg-4:handled',], result
@@ -30,7 +30,7 @@ def test_gevent_imap_zero_size():
     ''' test_greenlets.test_gevent_imap_zero_size
     '''
     try:
-        imap = imap_nonblocking(0, simple_task, Queue())        
+        imap = imap_nonblocking(simple_task, Queue(), 0)        
     except ValueError:
         pass
 
@@ -38,7 +38,7 @@ def test_gevent_imap_none_func():
     ''' test_greenlets.test_gevent_imap_none_func
     '''
     try:
-        imap = imap_nonblocking(5, None, Queue())        
+        imap = imap_nonblocking(None, Queue(), 5)        
     except ValueError:
         pass
 
