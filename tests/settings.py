3bf4fa668e503df07f248a6c7187c30c1ac0c13a
ownport <ownport@gmail.com>
2013-03-18 08:47:44 +0200

    configuration file for tests

---
+++ tests/settings.py
@@ -0,0 +1,25 @@
+#
+#   Settings
+#
+import os
+
+# During early development, the debug mode can be very helpful.
+# In this mode, Bottle is much more verbose and provides helpful 
+# debugging information whenever an error occurs. It also 
+# disables some optimisations that might get in your way and adds 
+# some checks that warn you about possible misconfiguration.
+# Here is an incomplete list of things that change in debug mode:
+#
+# - The default error page shows a traceback.
+# - Templates are not cached.
+# - Plugins are applied immediately.
+#
+# Just make sure not to use the debug mode on a production server.
+DEBUG_MODE = True
+
+# The absolute path to the logfile of http server
+HTTP_SERVER_LOGFILE = os.path.join(os.getcwd(), 'log/http_server.log')
+
+# The absolute path to the pidfile. It's required when http server is 
+# running as service
+HTTP_SERVER_PIDFILE = os.path.join(os.getcwd(), 'run/http_server.pid')
