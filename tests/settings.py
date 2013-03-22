f45151266c52a47238117794b207926a36a129ea
ownport <ownport@gmail.com>
2013-03-22 21:43:14 +0200

    update settings.py for echoserver.py support

--- tests/settings.py
+++ tests/settings.py
@@ -3,15 +3,16 @@
 #
 import os
 
-# Simple server IP address
-SIMPLE_SERVER_IP_ADDRESS = '127.0.0.1'
+# Echo server IP address
+ECHO_SERVER_IP_ADDRESS = '127.0.0.1'
 
-# Simple server IP port
-SIMPLE_SERVER_IP_PORT = 8800
+# Echo server IP port
+ECHO_SERVER_IP_PORT = 8800
 
-# The absolute path to the logfile of simple server
-SIMPLE_SERVER_LOGFILE = os.path.join(os.getcwd(), 'logs/simple_server.log')
+# The absolute path to the logfile of echo server
+ECHO_SERVER_LOGFILE = os.path.join(os.getcwd(), 'logs/echoserver.log')
 
-# The absolute path to the pidfile. It's required when simple server is 
+# The absolute path to the pidfile. It's required when echo server is 
 # running as service
-SIMPLE_SERVER_PIDFILE = os.path.join(os.getcwd(), 'run/simple_server.pid')
+ECHO_SERVER_PIDFILE = os.path.join(os.getcwd(), 'run/echoserver.pid')
+
