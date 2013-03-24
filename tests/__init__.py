f26f475ce525adc093e43db0a0417c6f2e438306
ownport <ownport@gmail.com>
2013-03-24 08:53:19 +0200

    added test cases for GreenletActors

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 from pyactors.actor import Actor 
 from pyactors.generator import GeneratorActor
 from pyactors.greenlets import GreenletActor
-from pyactors.greenlets import imap_nonblocking
+
+from gevent.queue import Queue as gQueue
+
 ''' 
 -------------------------------------------
 Basic Actors
@@ -99,32 +101,38 @@ Greenlets
 class TestGreenletActor(GreenletActor):
     ''' TestGreenletActor
     '''
-    def __init__(self, name=None, logger=None, iters=10):
+    def __init__(self, name=None, logger=None):
         super(TestGreenletActor, self).__init__(name=name, logger=logger)
-        self.iters = ['%s:%d' % (self.name, i) for i in range(iters)]
-        self.logger.debug('%s.__init__(), messages in queue: %s' % (self.name, self.iters))
+        self.empty_msg_counter = 0
+    
+    @staticmethod
+    def imap_job(message):
+        return 'imap_job:%s' % message
 
     def on_receive(self, message):
-        self.logger.debug('%s.on_receive(), messages in queue: %s' % (self.name, len(self.inbox)))
-        self.send(message)
-        self.logger.debug('%s.on_receive(), message: "%s" sent to itself' % (self.name, message))
-
+        ''' on_receive
+        '''
+        self.logger.debug('%s.on_receive(), sent message to imap_queue: %s' % (self.name, message))
+        self.imap_queue.put(message)
+        self.logger.debug('%s.on_receive(), messages in imap_queue: %d' % (self.name, len(self.imap_queue)))
+        
     def on_handle(self):
-        try:        
-            msg = self.iters.pop()
-            self.logger.debug('%s.on_handle(), messages in queue: %s' % (self.name, self.iters))
-        except IndexError:
-            self.logger.debug('%s.stop()' % self.name)
-            self.stop()
-            return
+        ''' on_handle
+        '''
+        self.logger.debug('%s.on_handle()' % (self.name,))
+        message = self.imap.next()
             
-        if self.parent is not None:
-            self.parent.send(msg)
-            self.logger.debug('%s.on_handle(), message "%s" sent to parent' % (self.name, msg))
+        if message:
+            if self.parent is not None:
+                self.logger.debug('%s.on_receive(), send "%s" to parent' % (self.name, message))
+                self.parent.send(message)
+            else:
+                self.logger.debug('%s.on_receive(), send "%s" to itself' % (self.name, message))
+                self.send(message)
         else:
-            self.send(msg)
-            self.logger.debug('%s.on_handle(), message "%s" sent to itself' % (self.name, msg))
-        self.logger.debug('%s.on_handle(), messages in child inbox: %s' % (self.name, len(self.inbox)))
-
-
+            self.empty_msg_counter += 1
+        
+        if self.empty_msg_counter > 10:
+            self.stop()
+                    
 
--- tests/test_greenlet_actors.py
+++ tests/test_greenlet_actors.py
@@ -4,32 +4,38 @@ if '' not in sys.path:
 
 import pyactors
 from pyactors.logs import file_logger
+from pyactors.greenlets import GeventInbox
 from pyactors.exceptions import EmptyInboxException
 
 from tests import TestGreenletActor as TestActor
+from tests import ParentGeneratorActor as ParentActor
 
 def test_family():
-    ''' test_generator_actors.test_family
+    ''' test_greenlet_actors.test_family
     '''
     actor = TestActor()
     assert actor.family == pyactors.actor.AF_GENERATOR
 
-def test_run():
+def test_run_actor():
     ''' test_greenlet_actors.test_run
     '''
     test_name = 'test_greenlet_actors.test_run'
     logger = file_logger(test_name, filename='logs/%s.log' % test_name) 
 
-    actor = TestActor(name='Actor',logger=logger)
-    actor.start()
-    pyactors.joinall([actor,])
+    parent = ParentActor(name='Parent',logger=logger)
+    child = TestActor(name='Child', logger=logger)
+    parent.add_child(child)
+    for i in range(10):
+        child.send(test_name)
+    parent.start()
+    pyactors.joinall([parent,])
 
     result = []
     while True:
         try:
-            result.append(actor.inbox.get())
+            result.append(parent.inbox.get())
         except EmptyInboxException:
             break
     assert len(result) == 10, result
-    assert set(result) == set(['Actor:%d' % i for i in range(10)]), result
+    assert result == ['imap_job:%s' % test_name for _ in range(10)], result
 
