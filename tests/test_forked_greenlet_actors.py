1047999424b97c304e8769ad5fb74c162d74fcb5
ownport <ownport@gmail.com>
2013-03-31 14:42:33 +0300

    added new test cases

--- tests/test_forked_greenlet_actors.py
+++ tests/test_forked_greenlet_actors.py
@@ -195,3 +195,111 @@ def test_processing_with_children():
                                'imap_job:Child-3:0','imap_job:Child-3:1','imap_job:Child-3:2', \
                                'imap_job:Child-4:0','imap_job:Child-4:1','imap_job:Child-4:2',]), result
 
+def test_processing_with_diff_timelife_children():
+    ''' test_forked_greenlet_actors.test_processing_with_diff_timelife_children
+    '''
+    test_name = 'test_forked_greenlet_actors.test_processing_with_diff_timelife_children'
+    logger = file_logger(test_name, filename='logs/%s.log' % test_name) 
+
+    parent = ForkedParentActor(name='Parent', logger=logger)      
+    for i in range(5):
+        child = EchoClientGreenletActor(name='Child-%s' % i, logger=logger)
+        for ii in range(i):
+            child.send((ECHO_SERVER_IP_ADDRESS, ECHO_SERVER_IP_PORT, '%s:%d' % (child.name, ii)))
+        parent.add_child(child)      
+    parent.start()
+    pyactors.joinall([parent,])
+
+    result = parent.inbox.dump()
+    assert len(result) == 10, result
+    assert set(result) == set(['imap_job:Child-1:0','imap_job:Child-2:0','imap_job:Child-2:1', \
+                               'imap_job:Child-3:0','imap_job:Child-3:1','imap_job:Child-3:2', \
+                               'imap_job:Child-4:0','imap_job:Child-4:1','imap_job:Child-4:2', \
+                               'imap_job:Child-4:3',]), result
+
+def test_send_msg_between_actors():
+    ''' test_forked_greenlet_actors.test_send_msg_between_actors
+    '''        
+    test_name = 'test_forked_greenlet_actors.test_send_msg_between_actors'
+    logger = file_logger(test_name, filename='logs/%s.log' % test_name) 
+
+    parent = ForkedParentActor(name='Parent', logger=logger)      
+    parent.add_child(SenderGreenletActor(name='Sender', logger=logger))      
+    parent.add_child(ReceiverGreenletActor(name='Receiver', logger=logger))      
+    parent.start()
+    pyactors.joinall([parent,])
+    assert parent.inbox.dump() == ['message from sender',]
+
+def test_send_stop_msg_to_child():
+    ''' test_forked_greenlet_actors.test_send_stop_msg_to_child
+    '''        
+    class Parent(pyactors.actor.Actor):
+        def __init__(self, name, logger):
+            ''' __init__
+            '''
+            super(Parent, self).__init__(name=name, logger=logger)
+            self.inbox = ProcessInbox()
+
+        def on_handle(self):
+        
+            for child in self.children:
+                self.logger.debug('%s.on_handle(), send "stop" message to child: %s' % (self.name, child))
+                child.send({'system-msg': {'type': 'stop', 'sender': self.address}})
+                
+            if len(self.children) == 0:
+                self.stop()
+                
+    test_name = 'test_forked_greenlet_actors.test_send_stop_msg_to_child'
+    logger = file_logger(test_name, filename='logs/%s.log' % test_name) 
+    parent = Parent(name='Parent', logger=logger)      
+    for i in range(5):
+        child = TestForkedGreenletActor(name='Child-%d' % i, logger=logger)
+        for i in range(3):
+            child.send(test_name)
+        parent.add_child(child)
+    parent.start()
+    pyactors.joinall([parent,])
+    
+    assert len(parent.inbox) > 0, 'parent.inbox: %s messages' % len(parent.inbox)
+
+def test_send_wrong_system_msg():
+    ''' test_forked_greenlet_actors.test_send_wrong_system_msg
+    '''        
+    class Parent(pyactors.actor.Actor):
+        def __init__(self, name, logger):
+            ''' __init__
+            '''
+            super(Parent, self).__init__(name=name, logger=logger)
+            self.inbox = ProcessInbox()
+
+        def on_handle(self):
+            ''' on_handle
+            '''
+            for child in self.children:
+                self.logger.debug('%s.on_handle(), send "wrong" system message to child: %s' % (self.name, child))
+                child.send({'system-msg': 'a1b2c3d4'})
+                
+            if len(self.children) == 0:
+                self.stop()
+                
+        def on_receive(self, message):
+            ''' on_receive
+            '''
+            self.logger.debug('%s.on_receive(), messages in inbox: %s' % (self.name, len(self.inbox)))
+            self.send(message)
+            self.logger.debug('%s.on_receive(), message: "%s" sent to itself' % (self.name, message))
+                
+    test_name = 'test_forked_greenlet_actors.test_send_wrong_system_msg'
+    logger = file_logger(test_name, filename='logs/%s.log' % test_name) 
+    parent = Parent(name='Parent', logger=logger)      
+    child = TestForkedGreenletActor(name='Child-0', logger=logger)
+    for i in range(3):
+        child.send('%s:%d' % (child.name, i))
+    parent.add_child(child)
+    parent.start()
+    pyactors.joinall([parent,])
+    
+    result = parent.inbox.dump()
+    assert len(result) == 3, 'parent.inbox: %s messages' % len(result)
+    assert set(result) == set(['imap_job:Child-0:0','imap_job:Child-0:1','imap_job:Child-0:2']), result
+
