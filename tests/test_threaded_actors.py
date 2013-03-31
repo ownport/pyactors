a8f9f55a2ce01f7288dd8c46b6cfdeb25a306709
ownport <ownport@gmail.com>
2013-03-31 10:45:57 +0300

    code refactoring

--- tests/test_threaded_actors.py
+++ tests/test_threaded_actors.py
@@ -76,9 +76,9 @@ def test_stop_children_in_the_middle():
     # threaded actor is started, actor's processing started in the thread and
     # parallel execution controlled by the system. In case of parent actor
     # sends stop message in the middle of execution, parent's inbox should
-    # contain more than 0 messages but not more that final resul:
+    # contain 0 or more messages but not more that final resul:
     # 5 children * 10 messages = 50
-    assert len(parent.inbox) > 0 and len(parent.inbox) < 50, \
+    assert len(parent.inbox) >= 0 and len(parent.inbox) < 50, \
             'parent inbox size: %s' % len(parent.inbox)
 
 def test_processing_with_diff_timelife_children():
