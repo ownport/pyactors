e782cb55c34700c8ec18eda9074c18743e3bd8a3
ownport <ownport@gmail.com>
2013-04-08 10:39:07 +0300

    update version

--- pyactors/__init__.py
+++ pyactors/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Andrey Usov <https://github.com/ownport/pyactors>'
-__version__ = '0.3.0-new-actor-flow' 
+__version__ = '0.3.0' 
 __license__ = """
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
